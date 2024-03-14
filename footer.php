d86e7b96391b85397431bc14458f228cf82aefba
AmandaOliveira01 <163453467+AmandaOliveira01@users.noreply.github.com>
2024-03-14 15:23:18 -0300

    Add files via upload
    Commit realizado com alterações aplicadas na aula do dia 14 de março

---
+++ footer.php
@@ -0,0 +1,7 @@
+</div>
+    <!-- Conateiner que abrigará o rodapé da Página -->
+    <div class="container-fluid text-center bg-dark text-light fixed-bottom">
+        <p>Sistema desenvolvido por Amanda</p>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include("header.php")?>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+        <h1>Aqui ficará o conteúdo da Página </h1>
+
+<?php include("footer.php")?>   
+        
+    
